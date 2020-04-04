667d51241b73eb93ea37a042ba77b33a289482ed
cjmartin20 <cjm086@shsu.edu>
2020-04-04 13:11:47 -0500

    function to create circle was made into class and put in it's own file

---
+++ AttributeBlocks/lib/circle.lua
@@ -0,0 +1,70 @@
+----------------------------------------------------------------------------------------
+--circle.lua Makes shapes that user can move
+----------------------------------------------------------------------------------------
+
+local circle = {
+    hasAttribute = nil,
+    "round",
+    "radius",
+    "circumference",
+    "no vertices"
+}
+
+local shapes = require "attributes"
+
+--function createCircle displays circle object and initializes circle.hasAttrube
+--based on currentAttribute in shapes.lua.
+function circle.createCircle( attribute )
+	local x = display.contentCenterX
+	local y = display.contentCenterY
+    local rad = 150
+	--make random colors (excluding green - setting to zero)
+	Red = 30
+	Green = 0
+	Blue = 150
+	aCircle = display.newCircle( x, y, rad )		--x-coordinate, y-coordinate, radius
+	aCircle:setFillColor( Red, Green, Blue )      -- fill the circle with color
+	aCircle.strokeWidth = 10   -- Sets the width of the border of circle
+	aCircle:setStrokeColor( 128, 0, 128 )    -- Sets the border color
+	physics.addBody( aCircle, {friction = 1.0, bounce = 0.7, radius = rad, setGravity = .5 })
+    aCircle:addEventListener( "touch", circle.move )
+    aCircle.alpha = 0.7 --circle opacity 
+    --check if cirlce has attributes.currentAttribute (in attributes.lua table)
+    for index, attribute in ipairs(circle) do
+        print("checking ", index, attribute)
+        if attribute == attributes.currentAttribute then
+            circle.hasAttribute = true
+            print("true")
+        end
+    end
+    --initialize attributes.hasAttribute if no value set it to true
+    if circle.hasAttribute == nil then
+        circle.hasAttribute = false
+    end
+end --createCircle function
+
+--Move shapes function
+function circle.move( event )
+
+	local object = event.target
+    local touchDistance = object.width
+
+	--Move shape
+	if math.abs( object.x - event.x ) < touchDistance and math.abs( object.y - event.y ) < touchDistance then
+		object.x = event.x
+		object.y = event.y
+    end
+    --Change color if circle is in moon and has attribute
+	if attributes.isShapeWithinRadius( object, .85 * display.contentCenterX, display.contentCenterX, display.contentCenterY) then
+        if circle.hasAttribute then
+			--change color to green
+			object:setFillColor( 0, 128 , 0)
+		else
+			--change color to red
+			object:setFillColor( 128, 0 , 0 )
+		end
+	else
+		object:setFillColor( 30, 0, 150 )
+    end 
+end --move function
+return circle
