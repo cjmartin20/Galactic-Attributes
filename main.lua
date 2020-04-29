cfe40734aef5201d1aa1473b7507cfe07e3ca923
cjmartin20 <cjm086@shsu.edu>
2020-04-29 15:31:54 -0500

    improved library loading

--- main.lua
+++ main.lua
@@ -3,9 +3,9 @@
 ---------------------------------------------------------------
 
 --change the path below to the lib directory
-local lib =  "C:\\Users\\cjmar\\Documents\\SHARED\\Software Engineering\\Lua\\Galactic Attributes\\lib\\"
---local lib =  ".\\lib" --trying to get this to work
-package.path = lib .. "?.lua;" .. package.path
+--change the path below to the lib directory
+local curDir =  'C:\\Users\\cjmar\\Documents\\SHARED\\Software Engineering\\Lua\\Galactic Attributes\\'
+package.path = curDir .. 'lib\\?.lua;' .. package.path
 
 local useGameSetup = require "gameSetup"
 
