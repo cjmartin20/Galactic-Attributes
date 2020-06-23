50d47fd5455146182e6fa1b25ba21530e8681cdf
cjmartin20 <cjm086@shsu.edu>
2020-06-23 02:01:27 -0500

    added numbers and shapes directory to package path

--- main.lua
+++ main.lua
@@ -6,6 +6,8 @@
 --change the path below to the lib directory
 local curDir =  'C:\\Users\\cjmar\\Documents\\SHARED\\x_Github\\Galactic-Attributes\\'
 package.path = curDir .. 'lib\\?.lua;' .. package.path
+package.path = curDir .. 'lib\\numbers\\?.lua;' .. package.path
+package.path = curDir .. 'lib\\shapes\\?.lua;' .. package.path
 
 local useGameSetup = require "gameSetup"
 
